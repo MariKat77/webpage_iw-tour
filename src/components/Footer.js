06f85a8283e6d67ecc0c498c183ccc06bb01f5b0
Mario <mario.1999.mk@gmail.com>
2025-04-04 11:52:39 +0200

    style: change opening hours and some code reformat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,46 +8,49 @@ import "../App.css";
 import { IconContext } from "react-icons";
 
 function Footer() {
-
   return (
     <>
-      <IconContext.Provider value={{ color: 'white', size: '2em' }}>
+      <IconContext.Provider value={{ color: "white", size: "2em" }}>
         <footer>
-            <div className="footerContainer">
-                <div className="footerInfo">
-                    <div className="footerColumn1">
-                        <h3>Iw-Tour Mechanika Samochodowa</h3>
-                        <p>
-                            ul. Główna 48,<br/> 
-                            42-620, Nakło Śląskie<br/>
-                            Tel: +48 508 369 998
-                        </p>
-                        <Link to="https://goo.gl/maps/7cFbPWgGRpiQd92m6" className="googleMapsLink">
-                            <PiIcons.PiMapPinLineBold />
-                        </Link>
-                    </div>
-                    <div className="footerColumn2">
-                        <h3>Godziny otwarcia</h3>
-                        <p>
-                            Poniedziałek - Piątek: 8:00 - 17:00<br/> 
-                            Sobota: 8:00 - 13:00<br/>
-                            Niedziela: Nieczynne
-                        </p>
-                    </div>
-                </div>
-                <div className="footerCopyright">
-                    <div className="footerCopyrightText">
-                        <p>Wszelkie prawa zastrzeżone.</p>
-                    </div>
-                    <div className="footerCopyrightText">
-                        <p>Copyright by &#169; 2023 Mariusz Katerla</p>
-                    </div>
-                    <div className="footerCopyrightText">
-                        <p>Realizacja: Mariusz Katerla</p>
-                    </div>
-                </div>
+          <div className="footerContainer">
+            <div className="footerInfo">
+              <div className="footerColumn1">
+                <h3>Iw-Tour Mechanika Samochodowa</h3>
+                <p>
+                  ul. Główna 48,
+                  <br />
+                  42-620, Nakło Śląskie
+                  <br />
+                  Tel: +48 508 369 998
+                </p>
+                <Link
+                  to="https://goo.gl/maps/7cFbPWgGRpiQd92m6"
+                  className="googleMapsLink"
+                >
+                  <PiIcons.PiMapPinLineBold />
+                </Link>
+              </div>
+              <div className="footerColumn2">
+                <h3>Godziny otwarcia</h3>
+                <p>
+                  Poniedziałek - Piątek: 8:00 - 17:00
+                  <br />
+                  Sobota i niedziela: Nieczynne
+                </p>
+              </div>
             </div>
-            
+            <div className="footerCopyright">
+              <div className="footerCopyrightText">
+                <p>Wszelkie prawa zastrzeżone.</p>
+              </div>
+              <div className="footerCopyrightText">
+                <p>Copyright by &#169; 2023 Mariusz Katerla</p>
+              </div>
+              <div className="footerCopyrightText">
+                <p>Realizacja: Mariusz Katerla</p>
+              </div>
+            </div>
+          </div>
         </footer>
       </IconContext.Provider>
     </>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,39 +1,44 @@
-import './Contact.css'
-
+import "./Contact.css";
 
 const Contact = () => {
-    return(
-      <>
-        <div className="slidingGallery">
-          <div className='page-wrapper'></div>
+  return (
+    <>
+      <div className="slidingGallery">
+        <div className="page-wrapper"></div>
+      </div>
+      <div className="contactContainer">
+        <h1>Kontakt</h1>
+        <div className="containerWorkHours">
+          <h2>Godziny otwarcia</h2>
+          <p>
+            Poniedziałek - Piątek: 8:00 - 17:00
+            <br />
+            Sobota i niedziela: Nieczynne
+          </p>
         </div>
-        <div className='contactContainer'>
-          <h1>Kontakt</h1>
-          <div className='containerWorkHours'>
-            <h2>Godziny otwarcia</h2>
-            <p>
-                Poniedziałek - Piątek: 8:00 - 17:00<br/> 
-                Sobota: 8:00 - 13:00<br/>
-                Niedziela: Nieczynne
-            </p>
-          </div>
-          <div className='containerAdress'>
-            <h2>Adres</h2>
-            <p>
-            ul. Główna 48,<br/> 
-            42-620, Nakło Śląskie<br/>
+        <div className="containerAdress">
+          <h2>Adres</h2>
+          <p>
+            ul. Główna 48,
+            <br />
+            42-620, Nakło Śląskie
+            <br />
             Tel: +48 508 369 998
-            </p>
-          </div>
-          <div className='containerNavigation'>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10164.760736695622!2d18.906417299999998!3d50.4375581!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47112bc7ddfc86b3%3A0x282035f6e6188e12!2sIw-Tour%20mechanika%20samochodowa!5e0!3m2!1spl!2spl!4v1692985642182!5m2!1spl!2spl" 
-            style={{border:0}} title='map' allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
-          </div>
+          </p>
         </div>
-      </>
-    
-    ); 
-  };
-  
-  export default Contact;
+        <div className="containerNavigation">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10164.760736695622!2d18.906417299999998!3d50.4375581!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47112bc7ddfc86b3%3A0x282035f6e6188e12!2sIw-Tour%20mechanika%20samochodowa!5e0!3m2!1spl!2spl!4v1692985642182!5m2!1spl!2spl"
+            style={{ border: 0 }}
+            title="map"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
